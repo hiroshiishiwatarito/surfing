38e6776592fcfc0a2807da7b0604be3747bf940f
石渡戸紘 <>
2019-10-20 15:48:31 +0900

    最初のコミット

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
 CarrierWave.configure do |config|
-  
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
